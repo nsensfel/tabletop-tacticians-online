066c4e849e2f6e8fc16ab96528f4b07030132bf1
Nathanaël Sensfelder <spamshield+github.com@segfault.tech>
2025-08-25 17:36:06 +0200

    Working ports.

--- client/public/online.tacticians.tabletop.js
+++ client/public/online.tacticians.tabletop.js
@@ -20,158 +20,129 @@ online.tacticians.tabletop.init =
 			Gren.Main.init
 			(
 				{
-					node: document.getElementById(node_id)
+					node: document.getElementById(node_id),
+					flags:
+						{
+							url_params: online.tacticians.tabletop.get_raw_url_params(),
+							local_values: online.tacticians.tabletop.get_raw_local_values()
+						},
+					taskPorts:
+						{
+							set_page_title:
+								async function (str)
+								{
+									online.tacticians.tabletop.set_page_title(str);
+
+									return "";
+								},
+							get_url_params:
+								async function (str)
+								{
+									return online.tacticians.tabletop.get_url_params();
+								},
+							set_url_params:
+								async function (str)
+								{
+									online.tacticians.tabletop.set_url_params(str);
+
+									return "";
+								},
+							clear_url_params:
+								async function ()
+								{
+									online.tacticians.tabletop.clear_url_params();
+
+									return "";
+								},
+							get_local_values:
+								async function (str)
+								{
+									return online.tacticians.tabletop.get_local_values();
+								},
+							set_local_values:
+								async function (str)
+								{
+									online.tacticians.tabletop.set_local_values(str);
+
+									return "";
+								},
+							clear_local_values:
+								async function ()
+								{
+									online.tacticians.tabletop.clear_local_values();
+
+									return "";
+								}
+						}
 				}
 			);
-
-		online.tacticians.tabletop.app.ports.local_store.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				online.tacticians.tabletop.local_store(request.key, request.value);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.local_read.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				return online.tacticians.tabletop.local_read(request.key);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.local_delete.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				online.tacticians.tabletop.local_delete(request.key);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.local_delete_all.subscribe
-		(
-			online.tacticians.tabletop.local_delete_all
-		);
-
-		online.tacticians.tabletop.app.ports.url_param_store.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				online.tacticians.tabletop.url_param_store(request.key, request.value);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.url_param_read.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				return online.tacticians.tabletop.url_param_read(request.key);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.url_param_read_all.subscribe
-		(
-			online.tacticians.tabletop.url_param_read_all
-		);
-
-		online.tacticians.tabletop.app.ports.url_param_delete.subscribe
-		(
-			function (json_request)
-			{
-				var request = JSON.parse(json_request);
-
-				online.tacticians.tabletop.url_param_delete(request.key);
-			}
-		);
-
-		online.tacticians.tabletop.app.ports.url_param_delete_all.subscribe
-		(
-			online.tacticians.tabletop.url_param_delete_all
-		);
-
-		online.tacticians.tabletop.app.ports.set_page_title.subscribe
-		(
-			online.tacticians.tabletop.set_page_title
-		);
 	};
 
-online.tacticians.tabletop.local_store =
-	function (key, value)
-	{
-		localStorage.setItem(key, value);
-	};
-
-online.tacticians.tabletop.local_read =
-	function (key)
-	{
-		return localStorage.getItem(key);
-	};
-
-online.tacticians.tabletop.local_read_all =
+online.tacticians.tabletop.get_local_values =
 	function ()
 	{
 		return JSON.stringify(localStorage);
 	};
 
-online.tacticians.tabletop.local_delete =
-	function (key)
+online.tacticians.tabletop.get_raw_local_values =
+	function ()
 	{
-		localStorage.removeItem(key);
+		return localStorage;
 	};
 
-online.tacticians.tabletop.local_delete_all =
-	function (key)
+online.tacticians.tabletop.set_local_values =
+	function (dict_as_str)
+	{
+		online.tacticians.tabletop.clear_local_values();
+
+		var dict = JSON.parse(dict_as_str);
+
+		for (key in dict)
+		{
+			localStorage.set(key, dict[key]);
+		}
+	};
+
+online.tacticians.tabletop.clear_local_values =
+	function ()
 	{
 		localStorage.clear();
 	};
 
-online.tacticians.tabletop.url_param_store =
-	function (key, value)
+online.tacticians.tabletop.set_url_params =
+	function (dict_as_str)
 	{
+		online.tacticians.tabletop.clear_url_params();
+
 		const url = new URL(location);
 
-		url.searchParams.set(key, value);
+		var dict = JSON.parse(dict_as_str);
+
+		for (key in dict)
+		{
+			url.searchParams.set(key, dict[key]);
+		}
 
 		history.pushState({}, "", url);
 	};
 
-online.tacticians.tabletop.url_param_read =
-	function (key)
-	{
-		const url = new URL(location);
-
-		return url.searchParams.get(key);
-	};
-
-online.tacticians.tabletop.url_param_read_all =
+online.tacticians.tabletop.get_url_params =
 	function ()
 	{
 		const url = new URL(location);
 
-		return JSON.stringify(url.searchParams);
+		return JSON.stringify(Object.fromEntries(url.searchParams));
 	};
 
-online.tacticians.tabletop.url_param_delete =
-	function (key)
+online.tacticians.tabletop.get_raw_url_params =
+	function ()
 	{
 		const url = new URL(location);
 
-		url.searchParams.delete(key);
-
-		history.pushState({}, "", url);
+		return Object.fromEntries(url.searchParams);
 	};
 
-online.tacticians.tabletop.url_param_delete_all =
+online.tacticians.tabletop.clear_url_params =
 	function ()
 	{
 		const url = new URL(location);
@@ -185,7 +156,7 @@ online.tacticians.tabletop.url_param_delete_all =
 	};
 
 online.tacticians.tabletop.set_page_title =
-	function (value)
+	async function (value)
 	{
 		document.title = value;
 	};
